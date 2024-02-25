9028695ce017b878398dfd7c0f4f0b18aad0dc9c
LeyReTu <optimystic75@hotmail.com>
2024-02-25 20:18:09 +0100

    Checkpoint 3

---
+++ Checkpoint 3/M2C3_PythonAssignment.py
@@ -0,0 +1,94 @@
+#Exercise_1
+
+my_sentence =  "Courage is a love affair with the unknown"
+
+my_number = 75
+
+my_colors = ['blue', 'purple', 'pink', 'yellow', 'green']
+
+it_rains = True
+
+
+#Exercise_2
+
+my_sentence =  "Courage is a love affair with the unknown"
+
+first_word = my_sentence[0:3]
+
+new_sentence = first_word
+
+print(new_sentence)
+
+
+#Exercise_3
+
+my_colors = ['blue', 'purple', 'pink', 'yellow', 'green']
+
+first_element = my_colors[0]
+
+print(first_element)
+
+
+
+#Exercise_4
+
+my_number = 75
+
+new_number = my_number + 10
+
+print(new_number)
+
+
+#Exercise_5
+
+my_colors = ['blue', 'purple', 'pink', 'yellow', 'green']
+
+first_element = my_colors[-1]
+
+print(first_element)
+
+
+#Exercise_6
+
+names = 'harry,alex,susie,jared,gail,conner'
+
+names_list = names.split()
+
+print(names_list)
+
+
+#Exercise_7
+
+my_sentence =  "Courage is a love affair with the unknown"
+
+all_words = my_sentence.split()
+
+word_one = all_words[0]
+
+print(word_one)
+
+cap_word = word_one.upper()
+
+print(cap_word)
+
+my_phrase = cap_word + ' is a love affair with the unknown'
+
+print(my_phrase)
+
+
+#Exercise_8
+
+my_number = '75'
+
+phrase = f"""Life begins at {my_number}"""
+
+print(phrase)
+
+
+#Exercise_9
+
+print("hello world")
+
+
+
+
